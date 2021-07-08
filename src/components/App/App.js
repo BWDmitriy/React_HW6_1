2ec71ff2da4f500e6c3c6be009550ea4721101b0
DmitriyZ <zl.dmitriy94@gmail.com>
2021-07-08 17:45:59 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import NameForm from '../NameForm';
+import Calculator from '../Calculator';
+
+function App() {
+
+return (
+  <div className="App">
+    <NameForm />
+    <Calculator />
+  </div>
+);
+
+}
+
+
+export default App;
